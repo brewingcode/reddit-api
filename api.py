122a8d98cc0688ea48876ac4169cfd5194f5ae7d
Alex <alex@brewingcode.net>
2016-09-30 16:40:09 -0700

    move all of live thread watching into watch_thread

--- api.py
+++ api.py
@@ -4,8 +4,6 @@ import requests
 import os
 import sys
 import json
-import arrow
-import time
 
 def unpack_creds():
     env_var = os.environ['REDDIT_CREDS']
@@ -25,20 +23,6 @@ def api_request(url):
     token = response.json()['access_token']
     headers = {"Authorization": "bearer "+token, "User-Agent": user_agent}
     return requests.get("https://oauth.reddit.com/"+url, headers=headers)
-
-def watch_thread(thread):
-    most_recent = 0
-    while True:
-        response = api_request('live/'+thread).json()
-        posts = response['data']['children']
-        posts.reverse()
-        for post in filter(lambda x: x['data']['created_utc'] > most_recent, posts):
-            most_recent = post['data']['created_utc']
-            body = post['data']['body']
-            author = post['data']['author']
-            ts = arrow.get(post['data']['created_utc'])
-            print('{} {} --{}'.format(ts, body.encode('utf-8'), author.encode('utf-8')))
-        time.sleep(5)
 
 if __name__ == '__main__':
     url_path = sys.argv[1] if len(sys.argv) > 1 else 'api/v1/me'
